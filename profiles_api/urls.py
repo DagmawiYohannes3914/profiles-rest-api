718ab9aa023790eb51aede09b2f4c5fb6dbfd644
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-06-24 09:32:06 +0300

    updating viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -5,7 +5,7 @@ from rest_framework.routers import DefaultRouter
 from .views import HelloApiView, HellowViewSet
 
 router = DefaultRouter()
-router.register('hello-viewset', HellowViewSet, base_name='hello-viewset')
+router.register('hello-viewset', HellowViewSet, basename='hello-viewset')
 
 urlpatterns = [
     path('hello-view/', HelloApiView.as_view()),
