479095cda9fee4a4b7b9a7ac971a5ce041f99851
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-07-01 15:55:40 +0300

    updating urls

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -2,11 +2,12 @@ from django.urls import path, include
 
 from rest_framework.routers import DefaultRouter
 
-from .views import HelloApiView, HelloViewSet, UserProfileViewSet, UserLoginApiView
+from .views import HelloApiView, HelloViewSet, UserProfileViewSet, UserLoginApiView, UserProfileFeedViewset
 
 router = DefaultRouter()
 router.register('hello-viewset', HelloViewSet, basename='hello-viewset')
 router.register('profile', UserProfileViewSet)
+router.register('feed', UserProfileFeedViewset)
 
 urlpatterns = [
     path('hello-view/', HelloApiView.as_view()),
